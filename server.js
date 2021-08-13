3dbed38a63e0d1937d08ad173dad999e116b30c3
jerry <jerry4905@hotmail.com>
2021-08-12 20:36:16 -0700

    helpers added

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-const express = require('express');
-// const routes = require('./routes');
-const sequelize = require('./config/connection');
 const path = require('path');
-const routes = require('./controllers/');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const session = require('express-session');
-
-const session = require('express-session');
-
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -23,25 +19,21 @@ const sess = {
   })
 };
 
-var cookieParser = require('cookie-parser');
-
-app.use(cookieParser());
 app.use(session(sess));
 
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on routes
-app.use(routes);
+app.use(require('./controllers/'));
 
-// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => 
-  console.log(`Now listening on port ${PORT}!`));
+  app.listen(PORT, () => console.log('Now listening'));
 });
